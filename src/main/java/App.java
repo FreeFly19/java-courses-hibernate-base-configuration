b232a80a21fa3b535cf43b207137d11c30789a6c
FreeFly19 <wmsystem2008@yandex.ru>
2017-03-20 18:46:53 +0200

    Added some operation examples

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import entity.Book;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -6,6 +8,25 @@ public class App {
         SessionFactory sessionFactory = new Configuration()
                 .configure("file.cfg.xml")
                 .buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+
+
+        session.beginTransaction();
+        Book book = new Book();
+        book.setName("Book 1");
+        session.persist(book);
+        session.getTransaction().commit();
+
+        session.beginTransaction();
+        Book book1 = session.get(Book.class, 1);
+        System.out.println(book1.getName());
+        session.getTransaction().commit();
+
+
+
+
+        session.clear();
 
         sessionFactory.close();
     }
--- src/main/java/entity/Book.java
+++ src/main/java/entity/Book.java
@@ -1,15 +1,14 @@
 package entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false)
+    private String name;
 
     public Integer getId() {
         return id;
@@ -17,5 +16,13 @@ public class Book {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
